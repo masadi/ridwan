68b23f5756b4bfcc4b4535a21d42ade752c6f5a0
masadi <masadi.com@gmail.com>
2023-07-29 10:27:48 +0700

    Update document.blade.php

--- resources/views/cetak/document.blade.php
+++ resources/views/cetak/document.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.cetak')
 @section('content')
 <div class="text-center">
-  <p><strong>SERTIFIKAT KEMITRAAN</strong><br> Nomor :</p>
+  <p><strong>SERTIFIKAT KEMITRAAN</strong><br> Nomor : 000/03/A1/LDSB/15055/2023</p>
   <p>Telah terdaftar sebagai Mitra/Reseler PT LINK DATA SUMBER BAROKAH (BAROKAHNET) <i>Internet Service Provider</i> (ISP) di Kab. Sumenep</p>
   <br>
   <p>PT. BLA BLA <br> Berkedudukan di : <br> alamat</p>
